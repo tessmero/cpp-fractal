3ed871bbb8c68e25e2df10bc7ec0eead9b4f1ecc
Oliver Tessmer <oliver.tessmer@gmail.com>
2020-01-31 13:28:21 -0500

    enforced C++ 11 compiler, added library nlohmannjson

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,15 +1,12 @@
 #include "graphics.h"
 #include "fractal.h"
+#include "json.hpp"
 #include <vector>
 
-using namespace std;
+// for convenience
+using json = nlohmann::json;
 
-// convenience function to declare vectors 
-// in a way that works with old compilers
-vector<float> vec(float* x, unsigned s){
-  vector<float> result(x, x + s); 
-  return result;
-}
+using namespace std;
 
 
 int main(int argc, char *argv[]) {
@@ -17,16 +14,16 @@ int main(int argc, char *argv[]) {
   // rshape (recursive shape) describes the fine details of the fractal
   // must be composed of horizontal and vertical segments
   // must have terminals at (0,0) and (1,0)
-  float rx[] = {0,.4,.4,.6,.6,1};
-  float ry[] = {0,0,.2,.2,0,0};
-  Shape rshape( vec(rx,6), vec(ry,6) );
+  vector<float> rx = {0,.4,.4,.6,.6,1};
+  vector<float> ry = {0,0,.2,.2,0,0};
+  Shape rshape( rx, ry );
 
   // bshape (base shape) describes the overall shape of the fractal
   // must be composed of horizontal and vertical segments
   // units are pixels
-  float bx[] = {100,100,412,412,100};
-  float by[] = {100,412,412,100,100};
-  Shape bshape( vec(bx,5), vec(by,5) );
+  vector<float> bx = {100,100,412,412,100};
+  vector<float> by = {100,412,412,100,100};
+  Shape bshape( bx, by );
 
   // complexity of the fractal
   unsigned depth = 4;
